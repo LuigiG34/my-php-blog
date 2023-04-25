a7c1c246a3b5f5624c8556f07740a333b29457d3
Luigi Gandemer <luigigandemer6@gmail.com>
2023-04-25 14:21:10 +0200

    alert() method and generateSlug() method

--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controllers;
+
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 
@@ -15,9 +16,56 @@ abstract class Controller
     public function __construct()
     {
         // Paramétré le dossier contenant nos templates twig
-        $this->loader = new FilesystemLoader(ROOT.'/templates');
+        $this->loader = new FilesystemLoader(ROOT . '/templates');
 
         //Paramétré environnement twig
         $this->twig = new Environment($this->loader);
+    }
+
+    /**
+     * Afficher un message après une requete
+     *
+     * @param string $type
+     * @param string $message
+     */
+    public function alert(string $class, string $message)
+    {
+        $_SESSION['alert'] = [
+            "class" => $class,
+            "msg" => $message
+        ];
+        return $_SESSION['alert'];
+    }
+
+    /**
+     * Generate slug from post's title
+     *
+     * @param string $title
+     */
+    public function generateSlug(string $text)
+    {
+        // replace non letter or digits by divider
+        $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+
+        // transliterate
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+        // remove unwanted characters
+        $text = preg_replace('~[^-\w]+~', '', $text);
+
+        // trim
+        $text = trim($text, '-');
+
+        // remove duplicate divider
+        $text = preg_replace('~-+~', '-', $text);
+
+        // lowercase
+        $text = strtolower($text);
+
+        if (empty($text)) {
+            return 'n-a';
+        }
+
+        return $text;
     }
 }
