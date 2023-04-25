7a5e6f072f3b5d7dd56306465b3f5e9401f6dd01
Luigi Gandemer <luigigandemer6@gmail.com>
2023-04-25 16:08:38 +0200

    Entity Article

---
+++ src/Entity/Articles.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace App\Entity;
+
+class Articles
+{
+    protected $id_article;
+    protected $chapo;
+    protected $contenu;
+    protected $created_at;
+    protected $updated_at;
+    protected $img;
+    protected $slug;
+    protected $id_categorie;
+    protected $id_utilisateur;
+
+
+    /**
+     * Get the value of id_article
+     */ 
+    public function getId_article()
+    {
+        return $this->id_article;
+    }
+
+    /**
+     * Set the value of id_article
+     *
+     * @return  self
+     */ 
+    public function setId_article($id_article)
+    {
+        $this->id_article = $id_article;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of chapo
+     */ 
+    public function getChapo()
+    {
+        return $this->chapo;
+    }
+
+    /**
+     * Set the value of chapo
+     *
+     * @return  self
+     */ 
+    public function setChapo($chapo)
+    {
+        $this->chapo = $chapo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of contenu
+     */ 
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+
+    /**
+     * Set the value of contenu
+     *
+     * @return  self
+     */ 
+    public function setContenu($contenu)
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of created_at
+     */ 
+    public function getCreated_at()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Set the value of created_at
+     *
+     * @return  self
+     */ 
+    public function setCreated_at($created_at)
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of updated_at
+     */ 
+    public function getUpdated_at()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * Set the value of updated_at
+     *
+     * @return  self
+     */ 
+    public function setUpdated_at($updated_at)
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of img
+     */ 
+    public function getImg()
+    {
+        return $this->img;
+    }
+
+    /**
+     * Set the value of img
+     *
+     * @return  self
+     */ 
+    public function setImg($img)
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of slug
+     */ 
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set the value of slug
+     *
+     * @return  self
+     */ 
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id_categorie
+     */ 
+    public function getId_categorie()
+    {
+        return $this->id_categorie;
+    }
+
+    /**
+     * Set the value of id_categorie
+     *
+     * @return  self
+     */ 
+    public function setId_categorie($id_categorie)
+    {
+        $this->id_categorie = $id_categorie;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id_utilisateur
+     */ 
+    public function getId_utilisateur()
+    {
+        return $this->id_utilisateur;
+    }
+
+    /**
+     * Set the value of id_utilisateur
+     *
+     * @return  self
+     */ 
+    public function setId_utilisateur($id_utilisateur)
+    {
+        $this->id_utilisateur = $id_utilisateur;
+
+        return $this;
+    }
+}
