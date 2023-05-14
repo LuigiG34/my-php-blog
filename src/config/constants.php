94148707b7813e47fef3ac2b7705baa39af8d306
Luigi Gandemer <luigigandemer6@gmail.com>
2023-05-14 19:57:57 +0200

    Symfony Insight fix issues

---
+++ src/config/constants.php
@@ -0,0 +1,3 @@
+<?php
+// On definie une constante contenant le dossier racine du projet.
+define('ROOT', __DIR__ . '/../..');
--- templates/base.php
+++ templates/base.php
@@ -18,7 +18,7 @@
 	<!-- Bootstrap icons-->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
 	<!-- Core theme CSS (includes Bootstrap)-->
-	<link href="/theme/css/styles.css" rel="stylesheet" />
+	<link href="/assets/css/styles.css" rel="stylesheet" />
 
 </head>
 
@@ -107,7 +107,7 @@
 	<!-- Bootstrap core JS-->
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 	<!-- Core theme JS-->
-	<script src="/theme/js/scripts.js"></script>
+	<script src="/assets/js/scripts.js"></script>
 </body>
 
 </html>
